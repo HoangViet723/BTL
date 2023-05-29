c20b18e1f850f8367e812a25d55f1eb33a8ec4be
HoangViet723 <hoangvietn313@gmail.com>
2023-05-29 12:33:04 +0700

    commit note

--- QuanLyBookStore/Models/HopDong.cs
+++ QuanLyBookStore/Models/HopDong.cs
@@ -6,26 +6,20 @@ namespace QuanLyBookStore.Models
     public class HopDong
     {
         [Key]
-        [Required(ErrorMessage ="Không được bỏ trống")]
         [Display(Name ="Mã hợp đồng")]
         public string? MaHD {get; set;}
 
-        [Required(ErrorMessage ="Không được bỏ trống")]
         [Display(Name ="Tên nhân viên")]
         public string? TenNV {get; set;}
-        [Display(Name ="Tên nhân viên")]
         [ForeignKey("TenNV")]
         public NhanVien? NhanVien {get; set;}
 
-        [Required(ErrorMessage ="Không được bỏ trống")]
         [Display(Name ="Ngày bắt đầu")]
         public string? DateStart {get; set;}
 
-        [Required(ErrorMessage ="Không được bỏ trống")]
         [Display(Name ="Ngày kết thúc")]
         public string? DateEnd {get; set;}
 
-        [Required(ErrorMessage ="Không được bỏ trống")]
         [Display(Name ="Lương")]
         public string? Luong {get; set;}
         
--- QuanLyBookStore/Models/Sach.cs
+++ QuanLyBookStore/Models/Sach.cs
@@ -4,19 +4,15 @@ namespace QuanLyBookStore.Models
     public class Sach
     {
         [Key]
-        [Required(ErrorMessage ="Không được bỏ trống")]
         [Display(Name ="Mã sách")]
         public string? MaSach {get; set;}
 
-        [Required(ErrorMessage ="Không được bỏ trống")]
         [Display(Name ="Tên sách")]
         public string? TenSach {get; set;}
 
-        [Required(ErrorMessage ="Không được bỏ trống")]
         [Display(Name ="Tên NXB")]
         public string? TenNXB {get; set;}
 
-        [Required(ErrorMessage ="Không được bỏ trống")]
         [Display(Name ="Giá")]
         public string? Gia {get; set;}
     }
